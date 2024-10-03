eecf4966dcef37414eaab2d5c44d831f3fcd759a
желешка <110609402+je11yy@users.noreply.github.com>
2024-10-03 18:07:51 +0300

    task_1_1 changed

--- task_1_1.py
+++ task_1_1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 coefficients_a = [
     [-1.0, -7.0, -3.0, -2.0],
     [-8.0, 1.0, -9.0, 0.0],
@@ -23,14 +25,18 @@ def combine_rows(matrix_a, matrix_b, identity_matrix, row_index_1, row_index_2, 
     identity_matrix[row_index_1] = [ (element_1 + element_2 * weight) for element_1, element_2 in zip(identity_matrix[row_index_1], identity_matrix[row_index_2]) ]
     matrix_b[row_index_1] += matrix_b[row_index_2] * weight
 
+
 def get_triangular_matrix(matrix_a, matrix_b, identity_matrix):
     length = len(matrix_b)
+    determinant = 1.0
+
     for column in range(length):
         # поиск максимального по модулю элемента в столбце
         current_row = None
         for row in range(column, len(matrix_a)):
             if current_row is None or abs(matrix_a[row][column]) > abs(matrix_a[current_row][column]):
                 current_row = row
+        
         # нет решения
         if current_row is None:
             raise ValueError()
@@ -38,12 +44,17 @@ def get_triangular_matrix(matrix_a, matrix_b, identity_matrix):
         if current_row != column:
             # переставляем строку с макс элементом выше
             swap_rows(matrix_a, matrix_b, identity_matrix, current_row, column)
+            determinant *= -1
         
+        determinant *= matrix_a[column][column]
+
         # нормализация строки
         divide_row(matrix_a, matrix_b, identity_matrix, column, matrix_a[column][column])
 
         for row in range(column + 1, len(matrix_a)):
             combine_rows(matrix_a, matrix_b, identity_matrix, row, column, -matrix_a[row][column])
+
+    return determinant
 
 def Gauss(matrix_a, matrix_b):
     matrix_x = [0 for elem in matrix_b]
@@ -54,15 +65,8 @@ def Gauss(matrix_a, matrix_b):
 def print_matrix(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix)):
-            print(format(matrix[i][j], ".6f"), end=" ")
+            print(format(matrix[i][j], ".3f"), end=" ")
         print("")
-
-# в треугольной матрице определитель - произведение всех элементов главной диагонали
-def find_determinant(matrix):
-    res = 1
-    for i in range(len(matrix)):
-        res *= matrix[i][i]
-    return res
 
 def get_identity_matrix(n):
     identity_matrix = [[0.0] * n for _ in range(n)]
@@ -81,10 +85,26 @@ def reverse_matrix(matrix, identity_matrix):
             matrix[row] = [ (element_1 + element_2 * weight) for element_1, element_2 in zip(matrix[row], matrix[column]) ]
             identity_matrix[row] = [ (element_1 + element_2 * weight) for element_1, element_2 in zip(identity_matrix[row], identity_matrix[column]) ]
 
+
+matrix = np.array(coefficients_a)
+
 identity_matrix = get_identity_matrix(len(coefficients_a))
-get_triangular_matrix(coefficients_a, coefficients_b, identity_matrix)
+determinant = get_triangular_matrix(coefficients_a, coefficients_b, identity_matrix)
 Gauss(coefficients_a, coefficients_b)
-print("determinant: " + str(format(find_determinant(coefficients_a), ".6f")))
+print()
+
+print("determinant: {0:.3f}".format(determinant))
+print("real determinant: {0:.3f}".format(np.linalg.det(matrix) ))
+print()
+
 print("reverse matrix: ")
 reverse_matrix(coefficients_a, identity_matrix)
 print_matrix(identity_matrix)
+print()
+print("real reverse matrix:")
+print_matrix(np.linalg.inv(matrix))
+
+print()
+print("mult matrix on reverse matrix:")
+result = np.matmul(matrix, np.array(identity_matrix))
+print_matrix(result)
